4f108587e793f23ae47cad1525b23a72161e43dc
Rodrigo <ro.gemaque16@gmail.com>
2021-07-22 14:29:38 -0300

    cart rails shopping

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
-Rails.configuration.stripe = {
-  :publishable_key => Rails.application.credentials.stripe[:stripe_publishable_key],
-  :secret_key      => Rails.application.credentials.stripe[:stripe_secret_key]
-}
+# Rails.configuration.stripe = {
+#   :publishable_key => Rails.application.credentials.stripe[:stripe_publishable_key],
+#   :secret_key      => Rails.application.credentials.stripe[:stripe_secret_key]
+# }
 
-Stripe.api_key = Rails.application.credentials.stripe[:stripe_secret_key]
+# Stripe.api_key = Rails.application.credentials.stripe[:stripe_secret_key]
